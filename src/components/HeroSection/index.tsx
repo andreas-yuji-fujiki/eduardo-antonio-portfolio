1fdeb3520a46f4fa1bc3d8a105be9d036b3d5998
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-05-18 21:03:11 -0300

    feat(/components/HeroSection): Title is not a image anymore, and also description.

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -6,45 +6,64 @@ import styles from './HeroSection.module.scss';
 
 // hero section component
 export function HeroSection() {
-
   // verify if dark mode is enabled
   const theme = useThemeStore((state) => state.theme);
-  const isDarkMode = theme === "dark";
+  const isDarkMode = theme === 'dark';
 
   // invert white from images to be black and black to be white
   const filterStyle = {
     filter: 'invert(1) hue-rotate(180deg)',
-    transition: 'filter 0.3s ease',
+    transition: 'filter 0.5s ease',
   };
 
   return (
-    <div className={styles.heroSectionWrapper}>
-      {/* informations */}
-      <section className={styles.heroWrapper}>
-        <img 
-          src={'/images/HeroSection/eduardoPresentation.svg'}
-          style={ isDarkMode ? undefined : filterStyle}
-          alt="Presentation" 
-        />
-        
-        <p style={ { color: 'var(--text-color)' } }>
-          An 18 years old brazilian ongoing 
-          <br /> backend software engineer!
-        </p>
-      </section>
-      
+    <>
+      <div className={styles.heroSectionWrapper}>
+        {/* informations */}
+        <section className={styles.heroWrapper}>
+
+          {/* presentation title */}
+          <h1 className={`${styles.presentationTitle}`}>
+            <span className={styles.hiIm}>
+              Hi, I'm
+            </span>
+            <span className={styles.name}>
+              Eduardo <br />
+              Antonio
+            </span>
+          </h1>
+
+          {/* desktop / tablets description */}
+          <p
+            className={`${styles.descriptionParagraph}`}
+          >
+            <span className={styles.notVisibleOnMobile}>
+              An 18 years old Brazilian <br />
+            </span> 
+            {' '}
+            ongoing software engineer!
+          </p>
+        </section>
+      </div>
       {/* aside shapes */}
-      <img 
-        src="/images/HeroSection/shape1.svg" 
-        className={`${styles.shape} ${styles.shape1}`}
-        style={isDarkMode ? undefined : filterStyle}
-      />
-      
-      <img 
-        src="/images/HeroSection/shape2.svg" 
-        className={`${styles.shape} ${styles.shape2}`}
-        style={isDarkMode ? undefined : filterStyle}
-      />
-    </div>
+      <div className={styles.shapesContainer}>
+
+        <div className={styles.shapeImageWrapper}>
+          <img
+            src="/images/HeroSection/shape1.svg"
+            className={`${styles.shape} ${styles.shape1}`}
+            style={isDarkMode ? undefined : filterStyle}
+          />
+        </div>
+
+        <div className={styles.shapeImageWrapper}>
+          <img
+            src="/images/HeroSection/shape2.svg"
+            className={`${styles.shape} ${styles.shape2}`}
+            style={isDarkMode ? undefined : filterStyle}
+          />
+        </div>
+      </div>
+    </>
   );
 }

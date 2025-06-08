22e11e87971346d08c244a0ea5015cf1a0f122a5
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-06-08 00:24:46 -0300

    restarted from scratch

--- src/types/components/HeroSection/SocialButtons/SocialButtons.d.ts
+++
@@ -1,3 +0,0 @@
-export interface SocialButtonsProps {
-    ghVisible?: boolean; 
-}
--- src/types/stores/useThemeTypes.ts
+++
@@ -1,7 +0,0 @@
-export type Theme = "light" | "dark";
-
-export type ThemeStore = {
-    theme: Theme;
-    setTheme: (theme: Theme) => void;
-    toggleTheme: () => void;
-} 
